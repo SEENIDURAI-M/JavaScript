db2a4ef8e22997e3d3295d8d04b8e0a5530b8c11
SEENIDURAI-M <123536521+SEENIDURAI-M@users.noreply.github.com>
2023-02-20 17:55:00 +0530

    Add files via upload

---
+++ if-else.js
@@ -0,0 +1,34 @@
+let num = 10;
+
+if (num > 0) {
+  console.log("Number is positive");
+} else {
+  console.log("Number is non-positive");
+}
+
+//Checking if a number is odd or even
+let num = 6;
+if (num % 2 === 0) {
+  console.log(num + " is even.");
+} else {
+  console.log(num + " is odd.");
+}
+
+//Checking if a number is positive, negative, or zero
+let num = -5;
+if (num > 0) {
+  console.log(num + " is positive.");
+} else if (num < 0) {
+  console.log(num + " is negative.");
+} else {
+  console.log(num + " is zero.");
+}
+
+//Checking if a number is between two values
+let num = 20;
+if (num >= 10 && num <= 30) {
+  console.log(num + " is between 10 and 30.");
+} else {
+  console.log(num + " is not between 10 and 30.");
+}
+

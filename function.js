97ff8f2a8609316c0fd41f56de9590fc030adb21
SEENIDURAI-M <123536521+SEENIDURAI-M@users.noreply.github.com>
2023-02-22 09:41:45 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,39 +1,31 @@
 function sayHello() {
   console.log("Hello, world!");
 }
-sayHello(); // Output: "Hello, world!"(Function declaration)
+sayHello(); 
 
 const sayHello = function() {
   console.log("Hello, world!");
 }
-sayHello(); // Output: "Hello, world!"(Funtion expression)
+sayHello(); 
 
 const sayHello = () => {
   console.log("Hello, world!");
 }
-sayHello(); // Output: "Hello, world!"(Arrow funtion)
-
-function withPrefix(prefix) {
-  return function(text) {
-    console.log(`${prefix} ${text}`);
-  }
-}
-const logWithPrefix = withPrefix("LOG:");
-logWithPrefix("Hello"); // Output: "LOG: Hello"(Higher-Order Function)
+sayHello(); 
 
 setTimeout(function() {
   console.log("Hello, world!");
-}, 1000); // Output: "Hello, world!" (Anonymous Function)
+}, 1000); 
 
 (function() {
   console.log("Hello, world!");
-})(); // Output: "Hello, world!"(Self-Invoking Function)
+})(); 
 
 //A function that takes two numbers as arguments and returns their sum
 function addNumbers(num1, num2) {
   return num1 + num2;
 }
-console.log(addNumbers(2, 3)); // Output: 5
+console.log(addNumbers(2, 3)); 
 
 //A function that takes an array of numbers and returns the average
 function calculateAverage(numbers) {
@@ -43,15 +35,4 @@ function calculateAverage(numbers) {
   }
   return sum / numbers.length;
 }
-console.log(calculateAverage([2, 4, 6])); // Output: 4
-
-//A function that takes an object and returns a string with the object's properties and values
-function objectToString(obj) {
-  let str = "";
-  for (let key in obj) {
-    str += `${key}: ${obj[key]}, `;
-  }
-  return str.slice(0, -2);
-}
-console.log(objectToString({name: "John", age: 30, city: "New York"})); // Output: "name: John, age: 30, city: New York"
-
+console.log(calculateAverage([2, 4, 6])); 
